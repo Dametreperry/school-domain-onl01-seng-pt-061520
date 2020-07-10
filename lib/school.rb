b9b982efef444d3fa8793b12ef7c6881b97f7741
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-10 23:48:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,30 @@
 
 class School
-  
-  attr_accessor:name, :roster
-  
-  def initailize (name)
-    @name = name
-    @roster = {}
-  end
-  
-  def add_student (name, grade)
-    roster[grade] ||= []
-    roster[grade] << name 
-  end
+attr_accessor:name, :roster
 
-  def grade(grade)
-    roster.each do |k, v| 
-      if k == grade
-        return v 
+ def initialize (name)
+   @name = name
+   @roster = {}
+ end 
+ 
+ def add_student (name,grade)
+    roster[grade] ||= []
+    roster[grade] << name
+ end 
+ 
+ def grade(grade)
+    roster.each do |g, n| 
+      if g == grade
+        return n 
       end 
     end 
  end
  
- def sort 
+def sort 
   new_hash = {}
-  roster.each do |k, v| 
-    new_hash[k] = v.sort 
+  roster.each do |g, n| 
+    new_hash[g] = n.sort 
   end 
   new_hash
 end 
-end
+end 
