cb144e9caa4d56c7527c3995cedff5d18bd9d10b
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-10 23:44:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,17 +14,17 @@ class School
   end
 
   def grade(grade)
-    roster.each do |k, v| 
-      if k == grade
-        return v 
+    roster.each do |g, n| 
+      if g == grade
+        return n 
       end 
     end 
  end
  
  def sort 
   new_hash = {}
-  roster.each do |k, v| 
-    new_hash[k] = v.sort 
+  roster.each do |g, n| 
+    new_hash[g] = n.sort 
   end 
   new_hash
 end 
