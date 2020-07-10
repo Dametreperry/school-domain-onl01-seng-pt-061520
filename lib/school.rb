a1b2d8754aff7e604cbbfc7ddb230346643af6fb
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-10 23:20:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initailize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student (name, grade)
+    roster[grade] ||= []
+    roster[grade] << name 
+  end
+
+def grade(grade)
+    roster.each do |k, v| 
+      if k == grade
+        return v 
+      end 
+    end 
+ end
